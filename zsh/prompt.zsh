7ec119b43aa586fdd7ee183460f9284080b37995
Simon Gregory <simon@helvector.org>
2012-10-25 12:21:17 +0100

    fixing wonky git prompting

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -17,7 +17,7 @@ git_dirty() {
 }
 
 git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+ ref=$(git symbolic-ref HEAD 2>/dev/null) || return
  echo "${ref#refs/heads/}"
 }
 
