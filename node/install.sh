ef930f6d8992a16c6a6e034189729a6e3385b340
Simon Gregory <simon@helvector.org>
2012-11-05 10:35:38 +0000

    going node, almost

---
+++ node/install.sh
@@ -0,0 +1,10 @@
+open http://nodejs.org/download/
+
+# Installing via the package means setting user privileges on
+#
+# /usr/local/lib/node
+# /usr/local/lib/node_modules
+#
+
+npm install -g coffee-script
+npm install -g grunt
