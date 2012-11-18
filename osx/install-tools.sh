5242083f32a4180d579e6ca1995fa9088595eba2
Simon Gregory <simon@helvector.org>
2012-11-18 13:37:24 +0000

    a list of tools and apps to install

---
+++ osx/install-tools.sh
@@ -0,0 +1,26 @@
+# A reminder more than anything:
+
+brew doctor #make sure all is ok
+
+brew install git
+brew install zsh
+brew install wget
+brew install rbenv
+brew install ruby-build
+brew install ohcount
+brew install p7zip
+
+# Apps to install
+
+open http://mac.github.com
+open http://macromates.com
+open http://www.sublimetext.com/2
+open http://www.versionsapp.com
+open http://www.kaleidoscopeapp.com
+open http://www.jetbrains.com/idea/
+open http://www.spotify.com/uk/
+open http://colloquy.info/downloads.html
+open http://www.getinvisiblehand.com
+
+# Quicklook - create ~/Library/QuickLook
+wget https://github.com/downloads/whomwah/qlstephen/QLStephen.qlgenerator.zip
