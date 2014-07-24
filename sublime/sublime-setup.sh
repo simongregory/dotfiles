38e1484760928219b0cb04255e0326f7f5b22b13
Simon Gregory <simon@helvector.org>
2014-07-24 10:52:05 +0100

    More sublime settings

--- sublime/sublime-setup.sh
+++ sublime/sublime-setup.sh
@@ -1,6 +1,9 @@
 PACKAGES="$HOME/Library/Application Support/Sublime Text 3/Packages"
+SUBLIME_DOT="$HOME/.dotfiles/sublime"
 USER_PACKAGE="$PACKAGES/User"
-USER_SETTINGS="$USER_PACKAGE/Preferences.sublime-settings"
+USER_SETTINGS="Preferences.sublime-settings"
+USER_KEY_MAP="Default (OSX).sublime-keymap"
+MD_SETTINGS="Markdown.sublime-settings"
 
 mkdir -p "$USER_PACKAGE"
 cd "$PACKAGES"
@@ -8,20 +11,26 @@ cd "$PACKAGES"
 # link subl command line tool to the app by using:
 #ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" subl
 
-# If sublime/or user has already created the user settings stub back it up
-if [[ -f "$USER_SETTINGS" && ! -L "${USER_SETTINGS}" ]] ; then
-  mv "$USER_SETTINGS" "$USER_SETTINGS.bak"
-fi
+function link() {
+  # If sublime/or user has already created the user settings stub back it up
+  if [[ -f "$1" && ! -L "${1}" ]] ; then
+    mv "$1" "$1.bak"
+  fi
 
-if [[ ! -L "$USER_SETTINGS" ]] ; then
-  ln -s "$HOME/.dotfiles/sublime/Preferences.sublime-settings" "$USER_SETTINGS"
-fi
+  if [[ ! -L "$1" ]] ; then
+    ln -s "$2" "$1"
+  fi
+}
 
-# Using https to avoid problems with proxies
+link "$USER_PACKAGE/$USER_SETTINGS" "$SUBLIME_DOT/$USER_SETTINGS"
+link "$USER_PACKAGE/$USER_KEY_MAP" "$SUBLIME_DOT/$USER_KEY_MAP"
+link "$USER_PACKAGE/$MD_SETTINGS" "$SUBLIME_DOT/$MD_SETTINGS"
+
+# Use https to avoid problems with proxies
 if [[ ! -d "Cucumber" ]]; then
   echo "Installing Cucumber Submlime package"
   #git clone https://github.com/npverni/cucumber-sublime2-bundle.git Cucumber
-  git clone git://github.com/drewda/cucumber-sublime2-bundle.git Cucumber
+  git clone https://github.com/drewda/cucumber-sublime-bundle.git Cucumber
 fi
 
 if [[ ! -d "ActionScript3" ]]; then
@@ -34,7 +43,7 @@ if [[ ! -d "Git" ]]; then
   git clone https://github.com/kemayo/sublime-text-2-git.git Git
 fi
 
-if [[ ! -d "CoffeeScript" ]]; then
+if [[ ! -d "CoffeeScript-Sublime-Plugin" ]]; then
   echo "Installing CoffeeScript Submlime package"
   git clone https://github.com/Xavura/CoffeeScript-Sublime-Plugin.git
 fi
