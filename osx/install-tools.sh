168bc0a1bfb7a4a7fd733ad6355d05ab364af34b
Simon Gregory <simon@helvector.org>
2016-11-24 09:28:14 +0000

    modernise

--- osx/install-tools.sh
+++ osx/install-tools.sh
@@ -5,11 +5,11 @@ brew doctor #make sure all is ok
 brew install git
 brew install zsh
 brew install wget
+brew install nodejs
 
 brew install rbenv
 brew install ruby-build
 
-brew install ohcount
 brew install p7zip
 brew install ctags
 brew install coreutils #gnu core utilities
@@ -18,22 +18,23 @@ brew install coreutils #gnu core utilities
 
 open http://mac.github.com
 open http://macromates.com
-open http://www.versionsapp.com
-open http://www.kaleidoscopeapp.com
-open http://www.jetbrains.com/idea/
+#open http://www.versionsapp.com
+#open http://www.kaleidoscopeapp.com
+#open http://www.jetbrains.com/idea/
 open http://www.spotify.com/uk/
-open http://colloquy.info/downloads.html
-open http://www.getinvisiblehand.com
-open http://www.sublimetext.com/3
-open http://wbond.net/sublime_packages/package_control
-open https://github.com/jbtule/cdto
+#open http://colloquy.info/downloads.html
+#open http://www.getinvisiblehand.com
+#open http://www.sublimetext.com/3
+#open http://wbond.net/sublime_packages/package_control
+#open https://github.com/jbtule/cdto
 open https://www.virtualbox.org/wiki/Downloads
 open http://panic.com/transmit/
 open http://bjango.com/mac/istatmenus/
 open https://atom.io
 
 # Quicklook - create ~/Library/QuickLook
-wget https://github.com/downloads/whomwah/qlstephen/QLStephen.qlgenerator.zip
+#wget https://github.com/downloads/whomwah/qlstephen/QLStephen.qlgenerator.zip
+open https://github.com/whomwah/qlstephen/releases
 
 # Other stuff
 open https://github.com/eaton/solarized # solarized colloquy theme
