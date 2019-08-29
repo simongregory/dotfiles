2c6abadff6cf3e34ee74f1ec86ceb490414bea93
Simon Gregory <simon.gregory@bbc.co.uk>
2019-08-29 10:14:20 +0100

    Add npm build and open shortcuts

--- win/Microsoft.PowerShell_profile.ps1
+++ win/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,8 @@ Set-Alias n npm
 function ns {npm start}
 function ncb {npm run clean; npm run build}
 function nr {Param($task) npm run $task}
+function nb {npm run build}
 
 function nip {cd $HOME\code\nip}
 function home {cd $HOME}
+function open {ii $args} #file:// paths are ok, other url schemes didn't work
