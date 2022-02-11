7f51959f5088a0f022ff0d531cf2f6949ba93fe2
Simon Gregory <simon.gregory@bbc.co.uk>
2022-02-11 18:21:07 +0000

    not as much tm

--- tm/tm_setup.sh
+++ tm/tm_setup.sh
@@ -3,18 +3,18 @@ BUNDLES="$HOME/Library/Application Support/Avian/Bundles"
 mkdir -p "$BUNDLES"
 cd "$BUNDLES"
 
-git clone https://github.com/jashkenas/coffee-script-tmbundle.git CoffeeScript.tmbundle
+#git clone https://github.com/jashkenas/coffee-script-tmbundle.git CoffeeScript.tmbundle
 git clone https://github.com/bomberstudios/Strip-Whitespace-On-Save.tmbundle.git Strip-Whitespace-On-Save.tmbundle
 git clone https://github.com/rspec/rspec-tmbundle.git RSpec.tmbundle
 git clone https://github.com/cucumber/cucumber-tmbundle.git Cucumber.tmbundle
 git clone https://github.com/mads379/Whitespace.tmbundle.git Whitespace.tmbundle
 git clone https://github.com/fiveisprime/Jasmine.tmbundle.git
 
-git clone https://github.com/simongregory/flex-tmbundle.git Flex.tmbundle
-git clone https://github.com/simongregory/flexunit.tmbundle.git FlexUnit.tmbundle
-git clone https://github.com/simongregory/flexpmd.tmbundle.git FlexPMD.tmbundle
-git clone https://github.com/simongregory/actionscript3-tmbundle.git ActionScript3.tmbundle
-git clone https://github.com/simongregory/actionscript3-robotlegs.tmbundle.git ActionScriptRobotlegs.tmbundle
-git clone https://github.com/simongregory/mockolate.tmbundle.git Mockolate.tmbundle
+# git clone https://github.com/simongregory/flex-tmbundle.git Flex.tmbundle
+# git clone https://github.com/simongregory/flexunit.tmbundle.git FlexUnit.tmbundle
+# git clone https://github.com/simongregory/flexpmd.tmbundle.git FlexPMD.tmbundle
+# git clone https://github.com/simongregory/actionscript3-tmbundle.git ActionScript3.tmbundle
+# git clone https://github.com/simongregory/actionscript3-robotlegs.tmbundle.git ActionScriptRobotlegs.tmbundle
+# git clone https://github.com/simongregory/mockolate.tmbundle.git Mockolate.tmbundle
 git clone https://github.com/simongregory/helvectorlight.tmbundle.git HelvectorLight.tmbundle
 git clone https://github.com/simongregory/markdown.tmbundle.git Markdown.tmbundle
