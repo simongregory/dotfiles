cd8af9a8bf942b1973673f1efb7d47ee5600e484
Simon Gregory <simon.gregory@bbc.co.uk>
2022-02-27 19:38:23 +0000

    move conf from env into config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,6 @@
+export LSCOLORS="exfxcxdxbxegedabagacad"
+export CLICOLOR=true
+
 fpath=($ZSH/zsh/functions $fpath)
 
 autoload -U $ZSH/zsh/functions/*(:t)
@@ -23,5 +26,9 @@ setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
+
+# don't expand aliases _before_ completion has finished
+#   like: git comm-[tab]
+#setopt complete_aliases
 
 zle -N newtab
