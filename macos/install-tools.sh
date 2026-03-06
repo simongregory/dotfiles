e6cc1912f0f666b4c60e2a36af80f42b49ec441c
Simon Gregory <simon.gregory@bbc.co.uk>
2026-03-06 15:05:28 +0000

    should really brewfile

--- macos/install-tools.sh
+++ macos/install-tools.sh
@@ -3,20 +3,14 @@
 brew doctor #make sure all is ok
 
 #brew install git
-#brew install zsh
 brew install fnm #fast node manager
 brew install starship #terminal prompt
-#brew install wget
 
 brew install rbenv
 brew install ruby-build
 
 brew install p7zip
-brew install remotemobprogramming/brew/mob
-#brew install ctags
-#brew install coreutils #gnu core utilities
-
-#brew tap homebrew/cask
+#brew install remotemobprogramming/brew/mob
 
 # Quick look plugins
 # https://github.com/sindresorhus/quick-look-plugins
@@ -24,11 +18,13 @@ brew install remotemobprogramming/brew/mob
 brew install --cask qlstephen
 
 # QL Plugins may be embargoed
-# list quarantine with: xattr -r ~/Library/QuickLook
-# remove quarantine with: xattr -d -r com.apple.quarantine ~/Library/QuickLook
+# list quarantine with: 
+#   xattr -r ~/Library/QuickLook
+# remove quarantine with:
+#.  xattr -d -r com.apple.quarantine ~/Library/QuickLook
 
 # Fonts to install
-
+#
 # For fonts that work with starship
 # FiraCode, Inconsolata - light versions
 open https://www.nerdfonts.com/
@@ -47,12 +43,23 @@ open https://code.visualstudio.com/download
 open https://www.mozilla.org/en-GB/firefox/new/
 open https://www.google.co.uk/chrome/
 open https://rectangleapp.com/
+open https://gpgtools.org
+
+# Tools to install
+
+open https://rustup.rs
 
 brew install --cask \
   docker \
   rectangle
 
+# Go BBC
+brew tap bbc/gobbc https://github.com/bbc/gobbc
+brew install gobbc
+
 # Chrome Extensions
-open -a "Google Chrome" "chrome://extensions"
+# open -a "Google Chrome" "chrome://extensions"
 # Talend API Tester - Free Edition
 # JSON Viewer & use dark theme
+# MultiLogin
+# GoFullPage Screen Capture - https://gofullpage.com/
