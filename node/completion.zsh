e3b51992945a108850d2345aa4016fc3b04dc856
Simon Gregory <simon@helvector.org>
2026-04-06 21:41:05 +0100

    no cosmos

--- node/completion.zsh
+++ node/completion.zsh
@@ -1,9 +1,5 @@
 #eval "$(npm completion 2>/dev/null)"
 if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
   source <(npm completion)
+  # pnpm completion zsh
 fi
-
-# Although handy this really kills startup time
-# for @bbc/cosmos-cli
-#eval "$(cosmos completion)"
-#source <(cosmos completion)
